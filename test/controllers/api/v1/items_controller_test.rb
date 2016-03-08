e391107598d441ab27e7a55d99057771ca53c8ca
Steven Olson <steveoscaro@gmail.com>
2016-03-08 09:32:51 -0700

    add item serializer and passing test for items#index api

--- test/controllers/api/v1/items_controller_test.rb
+++ test/controllers/api/v1/items_controller_test.rb
@@ -4,22 +4,28 @@ class Api::V1::ItemsControllerTest < ActionController::TestCase
 
   test "#index" do
     get :index, format: :json
-    binding.pry
-    items = JSON.parse(response.body)
-
-    assert_response :success
-    assert_equal 2, items.count
-  end
-
-  test "#show" do
-    id = Item.first.id
-
-    get :index, id: id, format: :json
 
     items = JSON.parse(response.body)
 
     assert_response :success
+
     assert_equal 2, items.count
+    assert_equal items.first["name"], "Heavy Cotton Pants"
+    assert_equal items.first["description"], "heavy when wet"
+    assert items.first["image_url"]
+    refute items.first["created_at"]
+    refute items.first["updated_at"]
   end
+
+  # test "#show" do
+  #   id = Item.first.id
+  #
+  #   get :index, id: id, format: :json
+  #
+  #   items = JSON.parse(response.body)
+  #
+  #   assert_response :success
+  #   assert_equal 2, items.count
+  # end
 
 end
